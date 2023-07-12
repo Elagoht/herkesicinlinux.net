1af5216cc97a420efc8092d3e2ba69a82c458f60
Furkan Baytekin <furkanbaytekin@gmail.com>
2023-07-12 21:36:24 +0300

    feat: terminal components

---
+++ components/Terminal.tsx
@@ -0,0 +1,29 @@
+import { FC, ReactNode } from "react"
+
+interface ChildrenProp {
+  children: ReactNode
+}
+interface TerminalProps extends ChildrenProp {
+  prompt?: string
+}
+
+export const Terminal: FC<TerminalProps> = ({ children, prompt = "user@PC:~$ " }) => {
+
+  return <div className="term">
+    <span className="prompt">{prompt}</span>{children}
+  </div>
+}
+
+
+export const Output: FC<ChildrenProp> = ({ children }) => {
+  return (
+    <div className="output">{children}</div>
+  )
+}
+
+export const Window: FC<ChildrenProp> = ({ children }: { children: ReactNode }) => {
+
+  return <div className="window">
+    {children}
+  </div>
+}
