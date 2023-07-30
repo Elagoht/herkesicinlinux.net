eb1495f945a4d447818382121b13909542bbf6a3
Furkan Baytekin <furkanbaytekin@gmail.com>
2023-07-30 05:49:02 +0300

    article: 30 dakikada markdown öğrenin

--- components/template/Mdx.tsx
+++ components/template/Mdx.tsx
@@ -5,18 +5,8 @@ type MdxProps = {
   code: string
 }
 
-const components = {
-  a: ({ ...props }) => (
-    <a
-      target="_blank"
-      rel="noreferrer"
-      {...props}
-    />
-  ),
-}
-
 export function Mdx({ code }: MdxProps) {
   const Component = useMDXComponent(code)
 
-  return <Component components={components} />
+  return <Component />
 }
